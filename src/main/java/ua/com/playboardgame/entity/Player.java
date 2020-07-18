ba99736011a3718419694e3be3f35d62dbb76f9b
Sergey Meshchankin <smeshchankin@gmail.com>
2020-07-18 22:38:19 +0300

    [7W-001] Add cards to player entity

--- src/main/java/ua/com/playboardgame/entity/Player.java
+++ src/main/java/ua/com/playboardgame/entity/Player.java
@@ -1,10 +1,14 @@
 package ua.com.playboardgame.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Player {
 
   private final long id;
   private final String name;
   private int coins;
+  private final Set<Card> cards = new HashSet<>();
 
   public Player(long id, String name) {
     this.id = id;
@@ -17,6 +21,19 @@ public class Player {
 
   public void setCoins(int coins) {
     this.coins = coins;
+  }
+
+  public Set<Card> getCards() {
+    return cards;
+  }
+
+  /**
+   * Method for adding Card to the player hand
+   * @param card Card object
+   * @return true - if card was successfully  added, false - if card is already exists in player hand
+   */
+  public boolean addCard(Card card) {
+    return cards.add(card);
   }
 
   @Override
--- src/test/java/ua/com/playboardgame/entity/PlayerTest.java
+++ src/test/java/ua/com/playboardgame/entity/PlayerTest.java
@@ -1,6 +1,8 @@
 package ua.com.playboardgame.entity;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,5 +26,31 @@ class PlayerTest {
   void testPlayerCoins() {
     player.setCoins(1);
     assertEquals(1, player.getCoins());
+  }
+
+  @Test
+  void testPlayerHasEmptyCardsSet() {
+    assertNotNull(player.getCards());
+    assertEquals(0, player.getCards().size(), "Player has empty cards set");
+  }
+
+  @Test
+  void testPlayerAddingCards() {
+    player.addCard(new Card(1, "Card 1"));
+    player.addCard(new Card(1, "Card 2"));
+    assertEquals(2, player.getCards().size());
+  }
+
+  @Test
+  void testPlayerAddingEqualsCards() {
+    boolean[] expected = new boolean[] { true, true, false, true };
+    boolean[] actual = new boolean[] {
+        player.addCard(new Card(1, "Card 1")),
+        player.addCard(new Card(1, "Card 2")),
+        player.addCard(new Card(1, "Card 2")),
+        player.addCard(new Card(1, "Card 3")),
+    };
+    assertArrayEquals(expected, actual, "Check Card adding result");
+    assertEquals(3, player.getCards().size());
   }
 }
